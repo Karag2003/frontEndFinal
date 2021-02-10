350e3949a636ac06103af85e83d25abb69f0c514
Karag2003 <78375040+Karag2003@users.noreply.github.com>
2021-02-10 09:54:55 -0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-        $("#cue").click(function(){
-                $(".CSSEdit").slideToggle("slow");
-        });
+  $("#scrt2").hide();
+  $("#scrt3").hide();
+  
+  $("#scrt1").click(function(){
+    $("#scrt2").show();
+    $("#scrt3").show();
 });
